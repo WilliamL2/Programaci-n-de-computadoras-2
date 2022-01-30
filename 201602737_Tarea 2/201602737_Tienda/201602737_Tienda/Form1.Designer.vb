a5a86c7dfcea1aeb7bbbac5f827f4ae0766908eb
WilliamL2 <98672178+WilliamL2@users.noreply.github.com>
2022-01-29 18:40:17 -0600

    Tarea 2

---
+++ 201602737_Tarea 2/201602737_Tienda/201602737_Tienda/Form1.Designer.vb
@@ -0,0 +1,189 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Btnaceptar = New System.Windows.Forms.Button()
+        Me.Btnlimpiar = New System.Windows.Forms.Button()
+        Me.Btnsalir = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Btnaceptar
+        '
+        Me.Btnaceptar.Location = New System.Drawing.Point(646, 141)
+        Me.Btnaceptar.Name = "Btnaceptar"
+        Me.Btnaceptar.Size = New System.Drawing.Size(94, 29)
+        Me.Btnaceptar.TabIndex = 0
+        Me.Btnaceptar.Text = "Aceptar"
+        Me.Btnaceptar.UseVisualStyleBackColor = True
+        '
+        'Btnlimpiar
+        '
+        Me.Btnlimpiar.Location = New System.Drawing.Point(646, 201)
+        Me.Btnlimpiar.Name = "Btnlimpiar"
+        Me.Btnlimpiar.Size = New System.Drawing.Size(94, 29)
+        Me.Btnlimpiar.TabIndex = 1
+        Me.Btnlimpiar.Text = "Limpiar"
+        Me.Btnlimpiar.UseVisualStyleBackColor = True
+        '
+        'Btnsalir
+        '
+        Me.Btnsalir.Location = New System.Drawing.Point(646, 263)
+        Me.Btnsalir.Name = "Btnsalir"
+        Me.Btnsalir.Size = New System.Drawing.Size(94, 29)
+        Me.Btnsalir.TabIndex = 2
+        Me.Btnsalir.Text = "Salir"
+        Me.Btnsalir.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(198, 141)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(109, 20)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Libras de Arroz"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(326, 141)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(106, 20)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Libras de Frijol"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(469, 145)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(118, 20)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Libras de Azucar"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(198, 172)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(51, 20)
+        Me.Label4.TabIndex = 6
+        Me.Label4.Text = "Q 2.00"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(326, 172)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(51, 20)
+        Me.Label5.TabIndex = 7
+        Me.Label5.Text = "Q 1.75"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(469, 172)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(51, 20)
+        Me.Label6.TabIndex = 8
+        Me.Label6.Text = "Q 2.50"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(326, 62)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(178, 20)
+        Me.Label7.TabIndex = 9
+        Me.Label7.Text = "ABARROTERIA GAMALIEL"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(198, 203)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(109, 27)
+        Me.TextBox1.TabIndex = 10
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(326, 203)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(109, 27)
+        Me.TextBox2.TabIndex = 11
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(469, 203)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(109, 27)
+        Me.TextBox3.TabIndex = 12
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Btnsalir)
+        Me.Controls.Add(Me.Btnlimpiar)
+        Me.Controls.Add(Me.Btnaceptar)
+        Me.Name = "Form1"
+        Me.Text = "201602737_Tienda"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Btnaceptar As Button
+    Friend WithEvents Btnlimpiar As Button
+    Friend WithEvents Btnsalir As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+End Class
---
+++ 201602737_Tarea 2/201602737_Tienda/201602737_Tienda/Form1.vb
@@ -0,0 +1,52 @@
+﻿Public Class Form1
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub Btnsalir_Click(sender As Object, e As EventArgs) Handles Btnsalir.Click
+        Dim resultado As Integer
+        resultado = MessageBox.Show("Quiere Salir", "Salir", MessageBoxButtons.YesNo, MessageBoxIcon.Question)
+        If (resultado = DialogResult.Yes) Then Close()
+
+
+    End Sub
+
+    Private Sub Btnlimpiar_Click(sender As Object, e As EventArgs) Handles Btnlimpiar.Click
+        TextBox1.Text = ""
+        TextBox2.Text = ""
+        TextBox3.Text = ""
+    End Sub
+
+    Private Sub Btnaceptar_Click(sender As Object, e As EventArgs) Handles Btnaceptar.Click
+        Dim arroz As Object
+        Dim frijol As Object
+        Dim azucar As Object
+        Dim subtotal As Object
+        Dim iva As Object
+        Dim parcial As Object
+        Dim Descuento1 As Object
+        Dim Descuento2 As Object
+        Dim total As Object
+        Dim factura As Object
+
+
+        If (TextBox1.Text = "") Then TextBox1.Text = "0"
+        If (TextBox2.Text = "") Then TextBox2.Text = "0"
+        If (TextBox3.Text = "") Then TextBox3.Text = "0"
+
+        arroz = Val(TextBox1.Text)
+        frijol = Val(TextBox2.Text)
+        azucar = Val(TextBox3.Text)
+
+        subtotal = arroz * 2 + frijol * 1.75 + azucar * 2.5
+        iva = subtotal * 0.12
+        parcial = subtotal + iva
+        Descuento1 = subtotal * 0.025
+        total = parcial - Descuento1
+        factura = "Subtotal: " + String.Concat(subtotal) + vbCr + "IVA: " + String.Concat(iva) + vbCr + "Parcial: " + String.Concat(parcial) + vbCr + "Descuento1: " + String.Concat(Descuento1) + vbCr + "Pago Final: " + String.Concat(total)
+        MessageBox.Show(factura, "Factura Cambiaria", MessageBoxButtons.OK, MessageBoxIcon.Information)
+
+
+    End Sub
+End Class
