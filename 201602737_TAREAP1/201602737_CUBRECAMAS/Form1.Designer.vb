b9ff30c98e9f5c6f3ed629ce11c4540b2863fccf
WilliamL2 <98672178+WilliamL2@users.noreply.github.com>
2022-02-17 21:09:26 -0600

    201602737_TAREAP1

---
+++ 201602737_TAREAP1/201602737_CUBRECAMAS/Form1.Designer.vb
@@ -0,0 +1,225 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Btntotal = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Txtcantidad = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Btnlimpiar = New System.Windows.Forms.Button()
+        Me.Btnsalir = New System.Windows.Forms.Button()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.Rbtnsmall = New System.Windows.Forms.RadioButton()
+        Me.Rbtnmedium = New System.Windows.Forms.RadioButton()
+        Me.Rbtnlarge = New System.Windows.Forms.RadioButton()
+        Me.Rbtnalgodon = New System.Windows.Forms.RadioButton()
+        Me.Rbtnseda = New System.Windows.Forms.RadioButton()
+        Me.Rbtnlona = New System.Windows.Forms.RadioButton()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Btntotal
+        '
+        Me.Btntotal.Location = New System.Drawing.Point(574, 229)
+        Me.Btntotal.Name = "Btntotal"
+        Me.Btntotal.Size = New System.Drawing.Size(75, 23)
+        Me.Btntotal.TabIndex = 0
+        Me.Btntotal.Text = "Total"
+        Me.Btntotal.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(388, 45)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(147, 17)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "CHUMPAS GAMALIEL"
+        '
+        'Txtcantidad
+        '
+        Me.Txtcantidad.Location = New System.Drawing.Point(574, 173)
+        Me.Txtcantidad.Name = "Txtcantidad"
+        Me.Txtcantidad.Size = New System.Drawing.Size(100, 22)
+        Me.Txtcantidad.TabIndex = 10
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(571, 127)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(64, 17)
+        Me.Label4.TabIndex = 11
+        Me.Label4.Text = "Cantidad"
+        '
+        'Btnlimpiar
+        '
+        Me.Btnlimpiar.Location = New System.Drawing.Point(574, 269)
+        Me.Btnlimpiar.Name = "Btnlimpiar"
+        Me.Btnlimpiar.Size = New System.Drawing.Size(75, 23)
+        Me.Btnlimpiar.TabIndex = 12
+        Me.Btnlimpiar.Text = "Limpiar"
+        Me.Btnlimpiar.UseVisualStyleBackColor = True
+        '
+        'Btnsalir
+        '
+        Me.Btnsalir.Location = New System.Drawing.Point(574, 311)
+        Me.Btnsalir.Name = "Btnsalir"
+        Me.Btnsalir.Size = New System.Drawing.Size(75, 23)
+        Me.Btnsalir.TabIndex = 13
+        Me.Btnsalir.Text = "Salir"
+        Me.Btnsalir.UseVisualStyleBackColor = True
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Rbtnlona)
+        Me.GroupBox1.Controls.Add(Me.Rbtnseda)
+        Me.GroupBox1.Controls.Add(Me.Rbtnalgodon)
+        Me.GroupBox1.Location = New System.Drawing.Point(316, 164)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(182, 176)
+        Me.GroupBox1.TabIndex = 14
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Material"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.Rbtnlarge)
+        Me.GroupBox2.Controls.Add(Me.Rbtnmedium)
+        Me.GroupBox2.Controls.Add(Me.Rbtnsmall)
+        Me.GroupBox2.Location = New System.Drawing.Point(73, 164)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(182, 176)
+        Me.GroupBox2.TabIndex = 15
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Tallas"
+        '
+        'Rbtnsmall
+        '
+        Me.Rbtnsmall.AutoSize = True
+        Me.Rbtnsmall.Location = New System.Drawing.Point(0, 31)
+        Me.Rbtnsmall.Name = "Rbtnsmall"
+        Me.Rbtnsmall.Size = New System.Drawing.Size(63, 21)
+        Me.Rbtnsmall.TabIndex = 16
+        Me.Rbtnsmall.TabStop = True
+        Me.Rbtnsmall.Text = "Small"
+        Me.Rbtnsmall.UseVisualStyleBackColor = True
+        '
+        'Rbtnmedium
+        '
+        Me.Rbtnmedium.AutoSize = True
+        Me.Rbtnmedium.Location = New System.Drawing.Point(0, 65)
+        Me.Rbtnmedium.Name = "Rbtnmedium"
+        Me.Rbtnmedium.Size = New System.Drawing.Size(78, 21)
+        Me.Rbtnmedium.TabIndex = 17
+        Me.Rbtnmedium.TabStop = True
+        Me.Rbtnmedium.Text = "Medium"
+        Me.Rbtnmedium.UseVisualStyleBackColor = True
+        '
+        'Rbtnlarge
+        '
+        Me.Rbtnlarge.AutoSize = True
+        Me.Rbtnlarge.Location = New System.Drawing.Point(0, 107)
+        Me.Rbtnlarge.Name = "Rbtnlarge"
+        Me.Rbtnlarge.Size = New System.Drawing.Size(66, 21)
+        Me.Rbtnlarge.TabIndex = 18
+        Me.Rbtnlarge.TabStop = True
+        Me.Rbtnlarge.Text = "Large"
+        Me.Rbtnlarge.UseVisualStyleBackColor = True
+        '
+        'Rbtnalgodon
+        '
+        Me.Rbtnalgodon.AutoSize = True
+        Me.Rbtnalgodon.Location = New System.Drawing.Point(0, 31)
+        Me.Rbtnalgodon.Name = "Rbtnalgodon"
+        Me.Rbtnalgodon.Size = New System.Drawing.Size(81, 21)
+        Me.Rbtnalgodon.TabIndex = 17
+        Me.Rbtnalgodon.TabStop = True
+        Me.Rbtnalgodon.Text = "Algodon"
+        Me.Rbtnalgodon.UseVisualStyleBackColor = True
+        '
+        'Rbtnseda
+        '
+        Me.Rbtnseda.AutoSize = True
+        Me.Rbtnseda.Location = New System.Drawing.Point(0, 65)
+        Me.Rbtnseda.Name = "Rbtnseda"
+        Me.Rbtnseda.Size = New System.Drawing.Size(62, 21)
+        Me.Rbtnseda.TabIndex = 18
+        Me.Rbtnseda.TabStop = True
+        Me.Rbtnseda.Text = "Seda"
+        Me.Rbtnseda.UseVisualStyleBackColor = True
+        '
+        'Rbtnlona
+        '
+        Me.Rbtnlona.AutoSize = True
+        Me.Rbtnlona.Cursor = System.Windows.Forms.Cursors.SizeNS
+        Me.Rbtnlona.Location = New System.Drawing.Point(0, 107)
+        Me.Rbtnlona.Name = "Rbtnlona"
+        Me.Rbtnlona.Size = New System.Drawing.Size(61, 21)
+        Me.Rbtnlona.TabIndex = 19
+        Me.Rbtnlona.TabStop = True
+        Me.Rbtnlona.Text = "Lona"
+        Me.Rbtnlona.TextAlign = System.Drawing.ContentAlignment.BottomRight
+        Me.Rbtnlona.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(983, 450)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.Btnsalir)
+        Me.Controls.Add(Me.Btnlimpiar)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Txtcantidad)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Btntotal)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Btntotal As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Txtcantidad As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Btnlimpiar As Button
+    Friend WithEvents Btnsalir As Button
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents Rbtnlona As RadioButton
+    Friend WithEvents Rbtnseda As RadioButton
+    Friend WithEvents Rbtnalgodon As RadioButton
+    Friend WithEvents Rbtnlarge As RadioButton
+    Friend WithEvents Rbtnmedium As RadioButton
+    Friend WithEvents Rbtnsmall As RadioButton
+End Class
---
+++ 201602737_TAREAP1/201602737_CUBRECAMAS/Form1.vb
@@ -0,0 +1,120 @@
+﻿Public Class Form1
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub Btnlimpiar_Click(sender As Object, e As EventArgs) Handles Btnlimpiar.Click
+        Txtcantidad.Text = ""
+        Rbtnsmall.Checked = False
+        Rbtnmedium.Checked = False
+        Rbtnlarge.Checked = False
+        Rbtnalgodon.Checked = False
+        Rbtnseda.Checked = False
+        Rbtnlona.Checked = False
+
+
+    End Sub
+
+    Private Sub Btnsalir_Click(sender As Object, e As EventArgs) Handles Btnsalir.Click
+        Dim resultado As Integer
+        resultado = MessageBox.Show("Quiere Salir", "Salir", MessageBoxButtons.YesNo, MessageBoxIcon.Question)
+        If (resultado = DialogResult.Yes) Then Close()
+    End Sub
+
+    Private Sub Btntotal_Click(sender As Object, e As EventArgs) Handles Btntotal.Click
+        Dim subtotalsa As Object
+        Dim subtotalss As Object
+        Dim subtotalsl As Object
+        Dim subtotalma As Object
+        Dim subtotalms As Object
+        Dim subtotalml As Object
+        Dim subtotalla As Object
+        Dim subtotalls As Object
+        Dim subtotalll As Object
+        Dim cantidad As Object
+        Dim total As Object
+        Dim factura As Object
+        Dim venta As Object
+        Dim costou As Object
+        Dim ventau As Object
+
+
+        subtotalsa = 0
+        subtotalsl = 0
+        subtotalss = 0
+        subtotalma = 0
+        subtotalml = 0
+        subtotalms = 0
+        subtotalla = 0
+        subtotalls = 0
+        subtotalll = 0
+
+        cantidad = Val(Txtcantidad.Text)
+        If (Txtcantidad.Text = "") Then Txtcantidad.Text = "0"
+
+        If Rbtnsmall.Checked And Rbtnalgodon.Checked Then
+            subtotalsa = cantidad * 2 * 15 + cantidad * 65.5
+        End If
+
+
+        If Rbtnsmall.Checked And Rbtnlona.Checked Then
+            subtotalsl = cantidad * 2 * 30.99 + cantidad * 65.5
+        End If
+
+        If Rbtnsmall.Checked And Rbtnseda.Checked Then
+            subtotalss = cantidad * 2 * 23.99 + cantidad * 65.5
+        End If
+
+
+
+        If Rbtnmedium.Checked And Rbtnalgodon.Checked Then
+            subtotalma = cantidad * 2.5 * 15 + cantidad * 85.99
+        End If
+
+
+        If Rbtnmedium.Checked And Rbtnlona.Checked Then
+            subtotalml = cantidad * 2.5 * 30.99 + cantidad * 85.99
+        End If
+
+
+        If Rbtnmedium.Checked And Rbtnseda.Checked Then
+            subtotalms = cantidad * 2.5 * 23.99 + cantidad * 85.99
+        End If
+
+
+        If Rbtnlarge.Checked And Rbtnalgodon.Checked Then
+            subtotalla = cantidad * 3 * 15 + cantidad * 99.99
+        End If
+
+
+        If Rbtnlarge.Checked And Rbtnlona.Checked Then
+            subtotalll = cantidad * 3 * 30.99 + cantidad * 99.99
+        End If
+
+
+        If Rbtnlarge.Checked And Rbtnseda.Checked Then
+            subtotalls = cantidad * 3 * 23.99 + cantidad * 99.99
+        End If
+
+
+        total = subtotalsa + subtotalss + subtotalsl + subtotalma + subtotalms + subtotalml + subtotalla + subtotalll + subtotalls
+        venta = total * 1.65
+        costou = total / cantidad
+        ventau = venta / cantidad
+
+        total = Math.Round(total, 2)
+        venta = Math.Round(venta, 2)
+        costou = Math.Round(costou, 2)
+        ventau = Math.Round(ventau, 2)
+
+        factura = "Cantidad de unidades:" + String.Concat(Txtcantidad.Text) + vbCr + "Costo total: Q" + String.Concat(total) + vbCr + "Venta total: Q" + String.Concat(venta) + vbCr + "Costo unitario: Q" + String.Concat(costou) + vbCr + "Precio Venta unitaria: Q" + String.Concat(ventau)
+
+        MessageBox.Show(factura, "Factura cambiaria", MessageBoxButtons.OK, MessageBoxIcon.Information)
+
+
+    End Sub
+
+    Private Sub GroupBox1_Enter(sender As Object, e As EventArgs) Handles GroupBox1.Enter
+
+    End Sub
+End Class
